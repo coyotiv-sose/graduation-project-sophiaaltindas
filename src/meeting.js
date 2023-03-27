1095e2126d7b0d1796f112fb11e0d6944ebb2ee9
sophiaaltindas <67739481+sophiaaltindas@users.noreply.github.com>
2023-03-27 22:40:13 +0000

    Add expiredMeeting method

--- src/meeting.js
+++ src/meeting.js
@@ -1,15 +1,62 @@
 class Meeting {
   //  comments = []
   attendees = []
+  // photo = []
 
-  constructor(name, location, date, createdBy) {
+  constructor(name, description, location, date, createdBy) {
     this.name = name
+    this.description = description
     this.location = location
     this.date = date
     this.createdBy = createdBy
 
     // this.attendees = [creator]
     // creator.meetings.push(this)
+  }
+  getAttendees() {
+    return this.attendees
+  }
+  getCreatedBy() {
+    return this.createdBy
+  }
+  getName() {
+    return this.name
+  }
+  getDescription() {
+    return this.description
+  }
+  getLocation() {
+    return this.location
+  }
+  getDate() {
+    return this.date
+  }
+  setName(name) {
+    this.name = name
+  }
+  setDescription(description) {
+    this.description = description
+  }
+  setLocation(location) {
+    this.location = location
+  }
+  setDate(date) {
+    this.date = date
+  }
+  addAttendee(user) {
+    this.attendees.push(user)
+  }
+  removeAttendee(user) {
+    this.attendees = this.attendees.filter(attendee => attendee !== user)
+  }
+  expiredMeeting(date) {
+    if (this.date > Date()) {
+      this.attendees.forEach(attendee => {
+        attendee.leaveMeeting(this)
+      })
+      return true
+    }
+    return false
   }
 }
 
