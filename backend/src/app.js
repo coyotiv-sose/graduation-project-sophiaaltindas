1fbc2e2b4b887e0084d522eb5518f76973688c49
sophiaaltindas <67739481+sophiaaltindas@users.noreply.github.com>
2023-04-18 21:21:31 +0000

    setup CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ var express = require('express')
 var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
+var cors = require('cors')
 
 var indexRouter = require('./routes/index')
 var usersRouter = require('./routes/users')
@@ -13,6 +14,8 @@ console.log(process.env.MONGODB_CONNECTION_STRING)
 require('./database-connection')
 
 var app = express()
+
+app.use(cors())
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
