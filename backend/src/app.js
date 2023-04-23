86b6ba346036b7e8f2decf3199e8ac6814b3edf7
sophiaaltindas <67739481+sophiaaltindas@users.noreply.github.com>
2023-04-23 22:21:30 +0000

    Install and configure connect-mongo and express-session

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,10 +4,26 @@ var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 var cors = require('cors')
+var session = require('express-session')
+var MongoStore = require('connect-mongo')
+
+const mongoose = require('mongoose')
 
 var indexRouter = require('./routes/index')
 var usersRouter = require('./routes/users')
 var meetingsRouter = require('./routes/meetings')
+var accountRouter = require('./routes/account')
+
+// requires the model with Passport-Local Mongoose plugged in
+const User = require('./models/user')
+const passport = require('passport')
+
+// use static authenticate method of model in LocalStrategy
+passport.use(User.createStrategy())
+
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
 require('dotenv').config()
 console.log(process.env.MONGODB_CONNECTION_STRING)
@@ -15,11 +31,45 @@ require('./database-connection')
 
 var app = express()
 
-app.use(cors())
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+)
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
+
+const clientPromise = mongoose.connection.asPromise().then(connection => (connection = connection.getClient()))
+
+app.use(
+  session({
+    secret: 'hyk?fTOpD5qrdr!rL',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      secure: process.env.NODE_ENV === 'production',
+      maxAge: 1000 * 60 * 60 * 24 * 15, // 15 days
+    },
+    store: MongoStore.create({ clientPromise, stringify: false }),
+    // mongoUrl: process.env.MONGODB_CONNECTION_STRING,
+  })
+)
+
+app.use(passport.session())
+
+app.use((req, res, next) => {
+  const numberOfVisits = req.session.numberOfVisits || 0
+  req.session.numberOfVisits = numberOfVisits + 1
+  req.session.history = req.session.history || []
+  req.session.history.push({ url: req.url, ip: req.ip })
+
+  console.log('session', req.session)
+
+  next()
+})
 
 app.use(logger('dev'))
 app.use(express.json())
@@ -30,6 +80,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
 app.use('/meetings', meetingsRouter)
+app.use('/account', accountRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
