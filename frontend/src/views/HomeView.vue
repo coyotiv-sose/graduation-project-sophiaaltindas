c4bc68474c3f8dcd50863b0bd15cb660a39c4d4f
sophiaaltindas <67739481+sophiaaltindas@users.noreply.github.com>
2023-05-10 22:11:52 +0000

    Redirect to home after successful user login

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,11 +1,29 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
 </script>
+<script>
+import axios from 'axios'
+import { useAccountStore } from '../stores/account'
+import { mapActions, mapState } from 'pinia'
 
-<template>
-  <main>
-    <Suspense>
-      <TheWelcome />
-    </Suspense>
-  </main>
+export default {
+  name: 'HomeView',
+    async mounted() {
+    await this.fetchUser()
+  },
+  methods: {
+    ...mapActions(useAccountStore, ['fetchUser', 'logout']),
+  },
+  computed: {
+    ...mapState(useAccountStore, ['user']),
+  }
+}
+</script>
+
+<template lang="pug">
+h2 Hello , {{ user?.name }}!
+h3 Below you can find the latest meetings:
+
+
+
 </template>
